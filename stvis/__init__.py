55a2276708713e791dbfef53c517df0ba855965d
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2022-04-07 13:25:30 -0600

    updated pv_static

--- stvis/__init__.py
+++ stvis/__init__.py
@@ -1,10 +1,21 @@
 import streamlit.components.v1 as components
+#def pv_static(fig,name='graph'):
+#  h1=fig.height
+#  h1=int(h1.replace('px',''))
+#  w1=fig.width
+#  w1=int(w1.replace('px',''))
+#  fig.show(name+'.html')
+#  return components.html(
+#    fig.html,height=h1+30, width=w1+30
+#       )
+
 def pv_static(fig,name='graph'):
-  h1=fig.height
-  h1=int(h1.replace('px',''))
-  w1=fig.width
-  w1=int(w1.replace('px',''))
-  fig.show(name+'.html')
-  return components.html(
-    fig.html,height=h1+30, width=w1+30
-       )
+    h1=fig.height
+    h1=int(h1.replace('px',''))
+    w1=fig.width
+    w1=int(w1.replace('px',''))
+    fig.write_html(name+'.html')
+    components.html(
+     fig.html,height=h1+30, width=w1+30
+          )
+    return
